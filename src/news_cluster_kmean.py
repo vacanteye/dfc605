524458f8cd636bf9e7c57795e873b12643f44cab
vacanteye <vacanteye@hotmail.com>
2020-06-24 18:03:07 +0900

    update

--- src/news_cluster_kmean.py
+++ src/news_cluster_kmean.py
@@ -1,4 +1,4 @@
-import sqlite3
+import sqlite3, pdb
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -44,61 +44,72 @@ def plot_dendrogram(model, **kwargs):
 
     # Plot the corresponding dendrogram
     dendrogram(linkage_matrix, **kwargs)
+
+# Configurations
+n_clusters = 50
+n_data_size = 2000
     
 # Load Data
-#stopwords = read_dic('../dat/stopwords_ko.txt')
-
 conn = sqlite3.connect('../dat/news_daily_20200501.db')
-#df = pd.read_sql_query('SELECT title,nouns FROM news_table LIMIT 100', conn)
-df = pd.read_sql_query('SELECT title,nouns FROM news_table', conn)
+df = pd.read_sql_query('SELECT title,nouns FROM news_table LIMIT {}'.format(n_data_size), conn)
 conn.close()
 
 titles = df['title'].tolist()
-docs = df['nouns'].tolist()
+#docs = df['nouns'].tolist()
+docs = df['title'].tolist()
 
 # Vectorizing
 vectorizer = CountVectorizer()
 
 # Clustering
-X = vectorizer.fit_transform(docs)
+bows = vectorizer.fit_transform(docs) # Bag of Words
 
 # Features
-names = vectorizer.get_feature_names()
+words = vectorizer.get_feature_names()
 
 # L2 normalizing
-X = normalize(X)
+X = normalize(bows)
 
-n_clusters = 50
-
-# Cluster with k-means
-y_kmeans = KMeans(n_clusters=n_clusters).fit(X)
+# Cluster with K-Means
+kmeans = KMeans(n_clusters=n_clusters).fit(X)
 
 # trained labels and cluster centers
+clusters = kmeans.labels_
+centers = kmeans.cluster_centers_
 
-clusters = y_kmeans.labels_.tolist()
-centroids = y_kmeans.cluster_centers_.tolist()
 
-articles = {'title': titles, 'cluster': clusters}
+articles = {'title': titles, 'cluster': clusters.tolist()}
 df = pd.DataFrame(articles, index=[clusters], columns=['title', 'cluster'])
 
-
 for i in range(n_clusters):
-    sub_frame = df.loc[df['cluster'] == i]
-    print("Cluster:{} Size:{}".format(i+1, len(sub_frame)))
-    title_list = '\n '.join(sub_frame['title'].values.tolist())
+
+    #select row with given cluster
+    cluster_frame = df.loc[df['cluster'] == i]  
+
+    #Frequent Words
+    cluster_indexes = np.where(clusters == i)
+    if len(cluster_indexes) != 0: cluster_indexes = cluster_indexes[0] #tuple to ndarray
+
+    count_vector = None
+    for index in cluster_indexes:
+        vector = bows[index].todense().getA().ravel()
+        count_vector = vector + vector if count_vector is not None else vector
+
+    cluster_keywords = []
+    if count_vector is not None:
+        word_indexes = count_vector.argsort()[::-1][:7] #top 5 word indexes
+        for word_index in word_indexes:
+            keyword = words[word_index]
+            count = count_vector[word_index]
+            cluster_keywords.append('{}({})'.format(keyword, count))
+
+    cluster_keywords = ','.join(cluster_keywords)
+
+    print("cluster:{} articles:{} keywords:{}".format(i+1, len(cluster_frame), cluster_keywords))
+
+    title_list = '\n '.join(cluster_frame['title'].values.tolist())
     print(' {}'.format(title_list))
     print()
 
-print()
-print('shape=', X.shape)
 print('title:{}, cluster:{}'.format(len(titles), n_clusters))
-
-'''
-model = AgglomerativeClustering(distance_threshold=0, n_clusters=None)
-model = model.fit(X.todense())
-plot_dendrogram(model, truncate_mode='level', p=3)
-plt.xlabel("Number of points in node (or index of point if no parenthesis).")
-plt.show()
-'''
-
 
